3b730d46406cfafe28b801d69145cb9a47ba0686
Amal K Philip <amalkaduppilphilip@gmail.com>
2026-02-25 15:50:27 +0530

    New

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("Success!!")
+function change(){
+    const mainTextelement = document.getElementById("chtxt");
+    if(mainTextelement.innerText === "Haa Haa it worked"){
+        mainTextelement.innerText = "Days lived calculator"
+    }
+    else{
+        mainTextelement.innerText = "Haa Haa it worked"
+    }
+}
